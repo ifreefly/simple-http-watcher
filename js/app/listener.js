dd0a1a2309551ec75bbb725f6f79f8846b97d0b8
zzdjk6 <zzdjk6@126.com>
2013-06-16 02:27:52 +0800

    now it can catch request

---
+++ js/app/listener.js
@@ -0,0 +1,14 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: zzdjk6
+ * Date: 13-6-15
+ * Time: 下午10:34
+ * To change this template use File | Settings | File Templates.
+ */
+define([],function(){
+    return {
+        onSendHeaders:function(details){
+            console.log(details);
+        }
+    };
+});
---
+++ js/main.js
@@ -0,0 +1,23 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: zzdjk6
+ * Date: 13-6-15
+ * Time: 下午9:51
+ * To change this template use File | Settings | File Templates.
+ */
+app = {
+    urlFilter: {
+        urls: [
+            "http://*/*",
+            "https://*/*"
+        ]
+    }
+};
+
+require(['app/listener'], function (listener) {
+    $(function () {
+        $("#button-start").click(function () {
+            chrome.webRequest.onSendHeaders.addListener(listener.onSendHeaders, app.urlFilter);
+        });
+    });
+});
